825380fd44b5d88e23189eab30a25c28056513ac
JuliamCosta <juuhcosta@outlook.com>
2020-02-19 17:32:36 -0300

    index de exercicios

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Exercicios da Julia Costa</title>    	
+    </head>
+
+    <body>
+        
+        <div>
+            <h1>Links das Meus Exercicios</h1>
+        </div>
+        <a href="https://exercicios-costa.herokuapp.com/form_agendamento.php">Barbearia com Bootstrap</a>
+    </body>
+</html>
